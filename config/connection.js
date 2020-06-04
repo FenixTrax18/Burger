32e58f4338ae4e5430384a4eb012616264001ca7
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-06-03 21:43:06 -0400

    updated vars

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "ec2-52-7-39-178.compute-1.amazonaws.com",
-  port: 5432,
-  user: "rvqkivukjbpmgo",
-  password: "ad9bd7a6e92b6efb5aed742cd3851679d21317ee8e89d3bd2d944b20b918be84",
-  database: "d4tog5ro211qhb"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "MySQL_P@ssw0rd",
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
