b8f6e4219d914d69ca7ae72a359b03f86e6613a7
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-06-07 19:48:50 -0400

    added end connection

--- config/connection.js
+++ config/connection.js
@@ -13,6 +13,11 @@ else{
   })
 }
 
+try{
+  connection.end()
+}catch{
+  console.log("Could not end connection.")
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
