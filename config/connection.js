dc091716fe2d97c8605ca2779cc2b6a64912462a
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-06-07 20:00:47 -0400

    fixed try/catch fix

--- config/connection.js
+++ config/connection.js
@@ -13,4 +13,17 @@ else{
   })
 }
 
+try{
+  connection.end();
+}catch{
+  console.log("Could not end connection.")
+}
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
 module.exports = connection;
