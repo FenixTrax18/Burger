441bc51763f9b43474910979577a366b8ee25a20
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-06-07 20:41:26 -0400

    removing connect function

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,15 @@
-var mysql = require("mysql");
-var connection;
+const mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-  var pool = mysql.createPool(process.env.JAWSDB_URL);
-  connection = pool.getConnection();
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
-
 else{
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: "MySQL_P@ssw0rd",
     database: "burgers_db"
-  });
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+  })
 }
 
 module.exports = connection;
