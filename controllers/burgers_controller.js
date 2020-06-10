22bb13658553d0a6625380a3ad4985b2dc01f3d6
FenixTrax18 <60018827+FenixTrax18@users.noreply.github.com>
2020-06-09 21:00:59 -0400

    minor formatting cleanup

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,7 +19,6 @@ router.get("/", function(req, res) {
     ], [
       req.body.burger_name, req.body.devoured
     ], function(result) {
-      // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
@@ -33,7 +32,6 @@ router.get("/", function(req, res) {
       devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
@@ -46,7 +44,6 @@ router.get("/", function(req, res) {
   
     burger.deleteOne(condition, function(result) {
       if (result.affectedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
